8edeb672e83b09282032f0c0aaf3d6159262ecdf
Arnob !! <78209407+Arnob17@users.noreply.github.com>
2021-11-18 20:06:52 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let lines = [
+        "Waves of Water!",
+        "Snixon is Vibing!",
+        "Aliya is spying!",
+        "Bijoy is glowing!",
+        "Nubah is busy!",
+        "Jim is watching naruto!",
+        "Arnob is singing!"
+]
+const text = document.getElementById("lines");
+      document.getElementById("lines").innerHTML = `${lines[Math.floor(Math.random() * lines.length)]}`;
