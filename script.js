9bbc421e7b4aaecd9f49216c296c086b72a2ca9f
snixon <94747670+snixu@users.noreply.github.com>
2021-11-20 19:25:22 +0600

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ let lines = [
         "Jim is watching naruto!",
         "Arnob is singing!",
         "Samir is flying",
-        "Moon is Glowing"
+        "Moon is Glowing",
+        "snixon is epic",
 ]
 const text = document.getElementById("lines");
       setInterval(async() => {
